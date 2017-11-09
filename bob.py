ab39d54e1380ae58aa2bb6bf41ff9c42c2836d51
David Haynes <dhaynes3@gmu.edu>
2017-11-09 16:12:31 -0500

    Basic basic basic example

--- bob.py
+++ bob.py
@@ -6,11 +6,26 @@ bob.py
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import simpy
+
 def main():
     """
     Call functions and alert the user as to what's going on.
     """
-    print("Hello World")
+    env = simpy.Environment()
+    env.process(car(env))
+    env.run(until=15)
+
+def car(env):
+    while True:
+        print('Start parking at %d' % env.now)
+        parking_duration = 5
+        yield env.timeout(parking_duration)
+
+        print('Start driving at %d' % env.now)
+        trip_duration = 2
+        yield env.timeout(trip_duration)
+
 
 # If executed, run main()
 if __name__ == '__main__':
