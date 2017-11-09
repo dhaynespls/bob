1091802669df3eedd84dcb2e7710bbb2dd0959d1
David Haynes <dhaynes3@gmu.edu>
2017-11-09 16:08:05 -0500

    Add in scaffolding for bob

---
+++ bob.py
@@ -0,0 +1,17 @@
+#!/bin/sh python
+"""
+bob.py
+"""
+# Future Imports
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+def main():
+    """
+    Call functions and alert the user as to what's going on.
+    """
+    print("Hello World")
+
+# If executed, run main()
+if __name__ == '__main__':
+    main()
